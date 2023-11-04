44da2eadb1df312b8bb02dde896b5bbbb1aeea2b
captain_686 <53248484+captain686@users.noreply.github.com>
2023-11-04 22:02:59 +0800

    Update diy_script.sh

--- diy_script.sh
+++ diy_script.sh
@@ -44,7 +44,7 @@ svn export https://github.com/immortalwrt/luci/branches/openwrt-18.06/applicatio
 # svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/services/msd_lite package/msd_lite
 
 # 科学上网插件
-git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
+svn export https://github.com/ysx88/openwrt-packages/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
